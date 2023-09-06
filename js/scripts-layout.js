2fe1f6bd5013e2d1b983113c4c00817ab198a21e
AdryanMaikel <adryanmaikel@gmail.com>
2023-09-06 18:46:42 -0300

    update css

---
+++ js/scripts-layout.js
@@ -0,0 +1,13 @@
+const selecionarinformado = document.getElementById('selecionar-informado')
+const text = document.getElementById('text')
+const option = document.getElementsByClassName('option')
+
+selecionarinformado.addEventListener("click", function() {
+  selecionarinformado.classList.toggle("active")
+})
+
+for(options of option){
+  options.onclick = function() {
+    text.innerHTML = this.textContent;
+  }
+}
