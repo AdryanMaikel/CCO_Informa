bd29d403cbc6c6a053382abcdcf7b13b5a1f3f47
AdryanMaikel <adryanmaikel@gmail.com>
2023-09-14 18:19:28 -0300

    implementando os motivos

--- js/data.js
+++ js/data.js
@@ -24,9 +24,8 @@ const motivies = [
   "Pneu Furado",
   "Problema com passageiro",
   "Problemas mecânicos",
-  "Roleta",
   "Tempo insuficiente",
-  "Validador",
+  "Validador/ Roleta",
   "Vandalismo",
   "Vistoria EPTC"
 ]
@@ -38,100 +37,25 @@ const problems = [
   "Carroceria - Outros",
   "Carroceria - Portas",
   "Elétrica - Alternador",
+  "Elétrica - Iluminação Externa",
   "Elétrica - Iluminação interna",
   "Elétrica - Letreiro",
   "Elétrica - Pane elétrica",
   "Elétrica - Sem arranque",
-  "Motor - Aquecimento Motor",
+  "Motor - Cigarra/Aquecimento",
+  "Motor - Cigarra/óleo motor",
   "Motor - Correias",
   "Motor - Sem Força",
   "Motor - Vazamento de água",
   "Motor - Vazamento de óleo Diesel",
   "Motor - Vazamento de óleo motor",
-  "Quebra na viagem anterior",
-  "Suspenção - Freio",
+  "Problemas na viagem anterior",
+  "Suspensão - Arriada",
   "Suspensão - Carro atravessado",
   "Suspensão - Embreagem / Caixa",
-  "Suspensão - Mola / Amortecedor",
+  "Suspensão - Freio",
   "Suspensão - Roda",
   "Suspensão - Vazamento de ar"
 ]
 
 export{ activies, motivies, problems }
-
-// function autocomplete(motivies) {
-//   return motivies.filter((value)=>{
-//     const valueLowerCase = value.toLowerCase()
-//     const motiviesLowerCase = motivies.toLowerCase()
-//     return valueLowerCase.includes(motiviesLowerCase)
-//   })
-// }
-// motive.addEventListener('input', ({ target }) => {
-//   const dadosDoCampo = target.value
-//   if(dadosDoCampo.length) {
-//     const autoCompleteValores = autocomplete(dadosDoCampo)
-//     sugestoes.innerHTML = `
-//     ${autoCompleteValores.map((value) => {
-//       return `<li>${value}</li>`
-//     }).join('')}`
-//   }else{
-//     sugestoes.innerHTML = `
-//     ${motivies.map((value) => {
-//       return `
-//       <li class="option_motive">
-//         <span class="text_motive">${value}</span>
-//       </li>`
-//     }).join('')}`
-//   }
-// })
-// function autocomplete(activity) {
-//   return activies.filter((value)=>{
-//     const valueLowerCase = value.toLowerCase()
-//     const activityLowerCase = activity.toLowerCase()
-//     return valueLowerCase.includes(activityLowerCase)
-//   })
-// }
-// const campo = document.querySelector('.campo')
-// const sugestoes = document.querySelector('.list_activities')
-
-// campo.addEventListener('input', ({ target }) => {
-//   const dadosDoCampo = target.value
-//   if(dadosDoCampo.length) {
-//     const autoCompleteValores = autocomplete(dadosDoCampo)
-//     sugestoes.innerHTML = `
-//     ${autoCompleteValores.map((value) => {
-//       return `<li>${value}</li>`
-//     }).join('')}`
-//   }else{
-//     sugestoes.innerHTML = `
-//     ${activies.map((value) => {
-//       return `
-//       <li class="options_activies">
-//         <span class="text_activies">${value}</span>
-//       </li>`
-//     }).join('')}`
-//   }
-// })
-
-/* Gerando activitys */
-// const activity = document.getElementById('div_activity')
-// const direction = document.getElementById('direction')
-// activity.addEventListener("click", function() {
-//   activity.classList.toggle("active")
-//   const activies = ["BC", "CB", "TT", "BB", "CC", "BT", "TB", "SG", "RC", "NS", "SN", "NL", "LN"]
-//   const list_activities = document.querySelector('.list_activities')
-//   list_activities.innerHTML = `
-//   ${activies.map((value) => {
-//     return `
-//     <li class="options_activies">
-//       <span class="text_activies">${value}</span>
-//     </li>`
-//   }).join('')}`
-//   const options_activies = document.getElementsByClassName('options_activies')
-//   for(options of options_activies){
-//     options.onclick = function() {
-//       direction.innerHTML = this.textContent
-//       activity.classList.remove("active")
-//     }
-//   }
-// })
--- js/scripts-generating-cco-informa.js
+++ js/scripts-generating-cco-informa.js
@@ -25,8 +25,162 @@ gerar_cco_informa.addEventListener("click", function(){
   cco_informa += "\n"
   
   cco_informa += `- ${table.value}, carro ${car.value}\n`
-  cco_informa += `- ${line.value} das ${time.value}, ${direction.innerText}`
+  cco_informa += `- ${line.value} das ${time.value}, ${direction.innerText}\n`
   
+  switch (text_event.innerText) {
+    case "foi":
+      return div_event.classList.add("active")
+    case "atrasada":
+      if(delay_event.value.length > 0) {
+        cco_informa += `- Viagem realizada com ${delay_event.value} minutos de atraso`
+      }else{
+        return delay_event.focus()
+      }
+    break
+    case "perdida":
+      cco_informa += `- Viagem não realizada`
+    break
+    case "realizada a frente":
+      if(location_event.value.length > 0){
+        cco_informa += `- Viagem realizada a partir da ${location_event.value}`
+      }else{
+        return location_event.focus()
+      }
+    break
+    case "interrompida":
+      if(location_event.value.length > 0){
+        cco_informa += `- Viagem interrompida na ${location_event.value}`
+      }else{
+        return location_event.focus()
+      }
+    break
+  }
+  cco_informa += "\n- Motivo: "
+
+  switch (motive.value) {
+    case "Acidente":
+      cco_informa += "Carro ter se envolvido em um acidente"
+    break;
+    case "Assalto":
+      cco_informa += "Carro ter sido assaltado"
+    break;
+    case "Atrasado":
+      cco_informa += "Atraso"
+    break;
+    case "Avaria":
+      cco_informa += "Carro ter sofrido avaria"
+    break;
+    case "Congestionamento":
+      
+    break;
+    case "Falta de Carro":
+      cco_informa += "Falta de carro"
+    break;
+    case "Falta de Tripulação":
+      cco_informa += "Falta de "+who_missing.innerText.toLowerCase()
+    break;
+    case "Pneu Furado":
+      cco_informa += "Pneu furado do carro "+car.value
+    break;
+    case "Problema com passageiro":
+      cco_informa += "problema com passageiro"
+    break;
+    case "Tempo insuficiente":
+      cco_informa += "tempo insuficiente para realizar viagem"
+    break;
+    case "Validador/ Roleta":
+      cco_informa += `Problemas no validador/roleta do carro ${car.value}`
+    break;
+    case "Vandalismo":
+      cco_informa += "Carro "+car.value+" ter sofrido vandalismo"
+    break;
+    case "Vistoria EPTC":
+      cco_informa += "Carro "+car.value+" ter sido recolhido pela EPTC"
+    break;
+    case "Problemas mecânicos":
+      switch (problem.value) {
+        case "Carroceria - Ar Condicionado":
+          cco_informa += "Problemas no ar-condicionado do carro "+car.value
+        break;
+        case "Carroceria - Elevador APD":
+          cco_informa += "Problemas no elevador APD do carro "+car.value
+        break;
+        case "Carroceria - Itens de segurança":
+          cco_informa += ""
+        break;
+        case "Carroceria - Limpador / Espelho":
+          cco_informa += "Problemas no limpador/espelho do carro "+car.value
+        break;
+        case "Carroceria - Outros":
+          cco_informa += ""
+        break;
+        case "Carroceria - Portas":
+          cco_informa += "Problemas nas portas do carro "+car.value
+        break;
+        case "Elétrica - Alternador":
+          cco_informa += "Problemas no alternador do carro "+car.value
+        break;
+        case "Elétrica - Iluminação interna":
+          cco_informa += "Problemas de iluminação interna no carro "+car.value
+        break;
+        case "Elétrica - Letreiro":
+          cco_informa += "Problemas no letreiro do carro "+car.value
+        break;
+        case "Elétrica - Pane elétrica":
+          cco_informa += "Pane elétrica do carro "+car.value
+        break;
+        case "Elétrica - Sem arranque":
+          cco_informa += "Carro "+car.value+" não pegar"
+        break;
+        case "Motor - Cigarra/Aquecimento":
+          cco_informa += "Carro "+car.value+" ter super aquecido"
+        break;
+        case "Motor - Cigarra/óleo motor":
+          cco_informa += "Carro "+car.value+" ter super aquecido"
+        break;
+        case "Motor - Correias":
+          cco_informa += "Problemas nas correias do motor do carro "+car.value
+        break;
+        case "Motor - Sem Força":
+          cco_informa += "Carro "+car.value+" estar sem força"
+        break;
+        case "Motor - Vazamento de água":
+          cco_informa += "Vazamento de água no carro "+car.value
+        break;
+        case "Motor - Vazamento de óleo Diesel":
+          cco_informa += "Vazamento de diesel no carro "+car.value
+        break;
+        case "Motor - Vazamento de óleo motor":
+          cco_informa += "Vazamento de óleo do motor no carro "+car.value
+        break;
+        case "Problemas na viagem anterior":
+          cco_informa += ""
+        break;
+        case "Suspensão - Arriada":
+          cco_informa += "Problemas na suspenção do carro "+car.value
+        break;
+        case "Suspensão - Carro atravessado":
+          cco_informa += "Carro "+car.value+" estar atravessado"
+        break;
+        case "Suspensão - Embreagem / Caixa":
+          cco_informa += "Problemas na embreagem do carro "+car.value
+        break;
+        case "Suspenção - Freio":
+          cco_informa += "Problemas nos freios do carro "+car.value
+        break;
+        case "Suspensão - Roda":
+          cco_informa += "Problemas nas rodas do carro "+car.value
+        break;
+        case "Suspensão - Vazamento de ar":
+          cco_informa += "Vazamento de ar no carro "+car.value
+        break;
+      }
+    break;
+  }
+
+
+
+
   const text_cco_informa = document.getElementById("text_cco_informa")
   text_cco_informa.innerText = cco_informa
   
