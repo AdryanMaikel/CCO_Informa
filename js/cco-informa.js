99280d0fe2df259991d26455144ca0c55d64afa8
AdryanMaikel <adryanmaikel@gmail.com>
2023-09-15 17:03:42 -0300

    criando primeira do formulario cco_informa

--- js/cco-informa.js
+++ js/cco-informa.js
@@ -2,3 +2,26 @@ const dark = document.getElementById("dark")
 dark.addEventListener("click", function() {
   document.querySelector("body").classList.toggle("dark")
 })
+
+const informed = document.getElementById("informed")
+informed.addEventListener("click", function(){
+  informed.classList.toggle("active")
+})
+
+const who_informed = document.getElementById("who_informed")
+who_informed.addEventListener("click", function(){
+  who_informed.classList.toggle("open")
+})
+const options_informed = document.getElementsByClassName("informed")
+const box_supervisor = document.getElementById("box_supervisor")
+for(option of options_informed){
+  option.onclick = function(){
+    text_informed.innerHTML = this.textContent
+    who_informed.classList.remove("open")
+    if(this.textContent == "Fiscal"){
+      return box_supervisor.classList.add("active")
+    }else{
+      return box_supervisor.classList.remove("active")
+    }
+  }
+}
---
+++ js/data.js
@@ -0,0 +1,61 @@
+const activies = [
+  "BC",
+  "CB",
+  "TT",
+  "BB",
+  "CC",
+  "BT",
+  "TB",
+  "SG",
+  "RC",
+  "NS",
+  "SN",
+  "NL",
+  "LN"
+]
+const motivies = [
+  "Acidente",
+  "Assalto",
+  "Atrasado",
+  "Avaria",
+  "Congestionamento",
+  "Falta de Carro",
+  "Falta de Tripulação",
+  "Pneu Furado",
+  "Problema com passageiro",
+  "Problemas mecânicos",
+  "Tempo insuficiente",
+  "Validador/ Roleta",
+  "Vandalismo",
+  "Vistoria EPTC"
+]
+const problems = [
+  "Carroceria - Ar Condicionado",
+  "Carroceria - Elevador APD",
+  "Carroceria - Itens de segurança",
+  "Carroceria - Limpador / Espelho",
+  "Carroceria - Outros",
+  "Carroceria - Portas",
+  "Elétrica - Alternador",
+  "Elétrica - Iluminação Externa",
+  "Elétrica - Iluminação interna",
+  "Elétrica - Letreiro",
+  "Elétrica - Pane elétrica",
+  "Elétrica - Sem arranque",
+  "Motor - Cigarra/Aquecimento",
+  "Motor - Cigarra/óleo motor",
+  "Motor - Correias",
+  "Motor - Sem Força",
+  "Motor - Vazamento de água",
+  "Motor - Vazamento de óleo Diesel",
+  "Motor - Vazamento de óleo motor",
+  "Problemas na viagem anterior",
+  "Suspensão - Arriada",
+  "Suspensão - Carro atravessado",
+  "Suspensão - Embreagem / Caixa",
+  "Suspensão - Freio",
+  "Suspensão - Roda",
+  "Suspensão - Vazamento de ar"
+]
+
+export{ activies, motivies, problems }
