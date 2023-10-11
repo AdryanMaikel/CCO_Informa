ce3ec5a9d7cbc0222d77e02795605b77e1b9b4b3
AdryanMaikel <adryanmaikel@gmail.com>
2023-10-11 18:31:33 -0300

    fechando aba de enviar para planilha

--- js/send_sheet.js
+++ js/send_sheet.js
@@ -35,7 +35,10 @@ const submit_form_send_sheets = (events) => {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(DATA)
-  }).then(()=>{ if(confirm("Enviado com Sucesso! Deseja abrir a planilha?")){window.open("https://docs.google.com/spreadsheets/d/1voKCp0MJOelI0-qv9s_m3VF5GUvkS4lbKyArA6NbmTs/edit#gid=1247343348")}})
+  }).then(()=>{
+    if(confirm("Enviado com Sucesso! Deseja abrir a planilha?")){window.open("https://docs.google.com/spreadsheets/d/1voKCp0MJOelI0-qv9s_m3VF5GUvkS4lbKyArA6NbmTs/edit#gid=1247343348")}
+    container_send_sheets.classList.remove("open")
+  })
 }
 const form_send_sheets = document.getElementById("form_send_sheets").addEventListener("submit", submit_form_send_sheets)
 
