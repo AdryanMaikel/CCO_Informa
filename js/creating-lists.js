8f301f5253b883fbaed2da27ed0dc97fa40ee3f1
AdryanMaikel <adryanmaikel@gmail.com>
2023-12-24 20:54:00 -0300

    criando classe para criar as listas contendo o metodo de autocompletar
    Co-authored-by: AdryanMaikel <maikeladryan@gmail.com>

--- js/creating-lists.js
+++
@@ -1,9 +0,0 @@
-import { supervisions, activies, events, motives, problems, operators } from "./lists.js"
-import { createInteractiveInput } from "./data.js"
-
-createInteractiveInput("who_informed", supervisions)
-createInteractiveInput("box_operator", operators)
-createInteractiveInput("box_direction", activies)
-createInteractiveInput("box_event", events)
-createInteractiveInput("box_motive", motives)
-createInteractiveInput("box_problems", problems)
